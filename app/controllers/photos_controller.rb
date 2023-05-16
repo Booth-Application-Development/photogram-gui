307fa15c2e0ab50807129a98c3d707077645fb4b
Harminder Singh <harminder.singh@chicagobooth.edu>
2023-05-16 20:38:33 +0000

    Good point

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,8 +10,69 @@ class PhotosController < ApplicationController
 
   def show
 
+    url_id = params.fetch("path_id")
+    
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+    
     render({ :template => "photo_templates/show.html.erb"})
   
   end
 
-end  
+
+def baii
+  the_id = params.fetch("toast_id")
+  matching_photos = Photo.where({ :id => the_id})
+  the_photo = matching_photos.at(0)
+
+  the_photo.destroy
+
+  #render({ :template => "photo_templates/baii.html.erb"})
+
+  redirect_to("/photos")
+  
+end
+
+def create
+  
+input_image = params.fetch("query_image")
+input_caption = params.fetch("query_caption")
+input_owner_id = params.fetch("query_owner_id")
+
+a_new_photo = Photo.new
+
+a_new_photo.image = input_image
+a_new_photo.caption = input_caption
+a_new_photo.owner_id = input_owner_id
+
+a_new_photo.save
+
+next_url = "/photos/" + a_new_photo.id.to_s
+redirect_to(next_url)
+
+end
+
+def update
+ 
+ the_id = params.fetch ("modify_id")
+
+ matching_photos = Photo.where ({ :d => the_id})
+ 
+ the_photo = matching_photos.at(0)
+
+ input_image = params.fetch("query_image")
+ input_caption = params.fetch("query_caption")
+
+ the_photo.image = input_image
+ the_photo.caption = input_caption
+
+ the_photo.save
+
+#  render({ :template => "photo_templates/update.html.erb"})
+
+next_url = "/photos/" + the_photo.id.to_s
+
+redirect_to(next_url)
+
+end
+end
